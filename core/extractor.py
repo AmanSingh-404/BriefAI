ce14efc4eb42482a2f55228c3c1bb96a46e0486a
Aman Singh <amanscode2005@gmail.com>
2026-06-29 11:34:15 +0530

    feat: implement LLM extractor module and integrate transcription testing script

---
+++ core/extractor.py
@@ -0,0 +1,27 @@
+# Actionale items, decision, questions
+
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough,RunnableLambda
+
+
+import os
+
+def get_llm() :
+    return ChatMistralAI(
+        model="mistral/mistral-small-latest",
+        mistral_api_key=os.getenv("MISTRAL_API_KEY") ,
+        temperature=0.2  
+    )
+
+def buils_chain(system_prompt:str):
+    llm = get_llm()
+    return (
+        RunnablePassthrough() |
+        RunnableLambda(lambda x : {"text":x}) |
+        ChatPromptTemplate.from_messages([
+            ("system",system_prompt),
+            ("human", "{text}"),
+        ]) | llm | StrOutputParser()
+    )
